8119960d2ed256fb7c6f12a9489c3f0ce625ac66
Robert Starsi <klevoo@gmail.com>
2014-12-17 20:09:05 +0200

    first spec passed

--- spec/all.rb
+++ spec/all.rb
@@ -1,16 +1,18 @@
 require 'docker'
+require 'pry'
 
 build_path = File.expand_path File.join(File.dirname(__FILE__), '..')
 image_tag = 'test_klevo/percona'
 
-describe "running it as a container" do
+describe "image" do
   before(:all) do
     # Build the image
     @image = Docker::Image.build_from_dir build_path, t: image_tag
   end
   
   it "should expose the mysql tcp port" do
-    expect(@image.json["container_config"]["ExposedPorts"]).to include("3306/tcp")
+    # binding.pry
+    expect(@image.json["Config"]["ExposedPorts"]).to include("3306/tcp")
   end
 
   after(:all) do
