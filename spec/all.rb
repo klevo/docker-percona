c14dbac8c1188f79fa19d1ff0e3d2e4aab0fff83
Robert Starsi <klevoo@gmail.com>
2014-12-18 21:22:45 +0200

    spec: starting a container

--- spec/all.rb
+++ spec/all.rb
@@ -2,7 +2,7 @@ require 'docker'
 require 'pry'
 
 build_path = File.expand_path File.join(File.dirname(__FILE__), '..')
-image_tag = 'test_klevo/percona'
+image_tag = 'klevo/percona'
 
 describe "image" do
   before(:all) do
@@ -11,11 +11,32 @@ describe "image" do
   end
   
   it "should expose the mysql tcp port" do
-    # binding.pry
     expect(@image.json["Config"]["ExposedPorts"]).to include("3306/tcp")
   end
 
   after(:all) do
-    # @image.remove
+  end
+end
+
+describe "running container" do
+  before(:all) do
+    @image = Docker::Image.build_from_dir build_path, t: image_tag
+  end
+
+  it "will start and run mysql daemon" do
+    @container = Docker::Container.create(
+      'Image' => image_tag, 
+      'Detach' => true, 
+      'Env' => [ 'MYSQL_ROOT_PASSWORD=something' ]
+    )
+    @container.start
+    root_my_cnf = @container.exec(['bash', '-c', 'cat /root/.my.cnf'])
+    expect(root_my_cnf.first.first).to match(/password=something/)
+    
+    # binding.pry
+  end
+
+  after(:all) do
+    @container.delete(force: true)
   end
 end
