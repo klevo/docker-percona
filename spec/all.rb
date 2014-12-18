714d253ab910b9302eccd399be2a02fbd52396b4
Robert Starsi <klevoo@gmail.com>
2014-12-18 21:30:51 +0200

    specs cleaned up and they remove the test container

--- spec/all.rb
+++ spec/all.rb
@@ -3,37 +3,27 @@ require 'pry'
 
 build_path = File.expand_path File.join(File.dirname(__FILE__), '..')
 image_tag = 'klevo/percona'
+IMAGE = Docker::Image.build_from_dir build_path, t: image_tag
 
 describe "image" do
-  before(:all) do
-    # Build the image
-    @image = Docker::Image.build_from_dir build_path, t: image_tag
-  end
-  
   it "should expose the mysql tcp port" do
-    expect(@image.json["Config"]["ExposedPorts"]).to include("3306/tcp")
-  end
-
-  after(:all) do
+    expect(IMAGE.json["Config"]["ExposedPorts"]).to include("3306/tcp")
   end
 end
 
 describe "running container" do
   before(:all) do
-    @image = Docker::Image.build_from_dir build_path, t: image_tag
-  end
-
-  it "will start and run mysql daemon" do
     @container = Docker::Container.create(
       'Image' => image_tag, 
       'Detach' => true, 
       'Env' => [ 'MYSQL_ROOT_PASSWORD=something' ]
     )
     @container.start
+  end
+
+  it "has root .my.cnf file that contains the password specified on container create" do
     root_my_cnf = @container.exec(['bash', '-c', 'cat /root/.my.cnf'])
     expect(root_my_cnf.first.first).to match(/password=something/)
-    
-    # binding.pry
   end
 
   after(:all) do
