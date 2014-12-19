0f06f36bb628ae7da8b2921875e22cd1ad35d1cc
Robert Starsi <klevoo@gmail.com>
2014-12-19 18:13:41 +0200

    passing spec: it "can be stopped, started again and data survives"

--- spec/all.rb
+++ spec/all.rb
@@ -50,7 +50,8 @@ describe "running a container without attached volumes" do
   end
 end
 
-describe "running a container with empty attached volume" do
+# Note: we're using a mounted volume located within the host vm, because if we would mount something from OS X, we would have write permission problems: https://github.com/boot2docker/boot2docker/issues/581
+describe "running a container with mounted volume" do
   before :all do
     `boot2docker ssh "mkdir /tmp/empty-data-dir"`
     
@@ -71,14 +72,21 @@ describe "running a container with empty attached volume" do
     expect(stdout.first).to match(/mysql/)
     expect(stdout.first).to match(/information_schema/)
     expect(stdout.first).to_not match(/test/)
+    expect(stdout.first).to_not match(/survive/)
+  end
+  
+  # the following also tests the case where we mount an already existing data dir
+  it "can be stopped, started again and data survives" do
+    @container.exec(['bash', '-c', 'mysql -e "create database survive;"'])
+    @container.restart
+    # Wait for mysql to start
+    @container.exec(['bash', '-c', 'mysqladmin --silent --wait=30 ping'])
+    stdout, stderr = @container.exec(['bash', '-c', 'mysql -e "show databases;"'])
+    expect(stdout.first).to match(/survive/)
   end
   
   after :all do
     @container.delete(force: true)
     `boot2docker ssh "sudo rm -rf /tmp/empty-data-dir"`
   end
-end
-
-describe "running a container with populated attached volume" do
-  # TODO
 end
