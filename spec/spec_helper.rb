02c07758ec9fd6af4d64b816dad630ec0492579d
Robert Starsi <klevoo@gmail.com>
2015-05-04 22:01:33 +0200

    ruby 2.2.2, bundle update

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'docker'
 require 'pry'
 
+Excon.defaults[:ssl_verify_peer] = false
+
 DOCKERFILE_ROOT = File.expand_path File.join(File.dirname(__FILE__), '..')
 IMAGE_TAG = 'klevo/percona'
 IMAGE = Docker::Image.build_from_dir DOCKERFILE_ROOT, t: IMAGE_TAG
